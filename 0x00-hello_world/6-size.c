46edf8517f4c23c3879fa23a6d3fb7c079c996b8
ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-03-16 10:04:46 -0700

    size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: always return 0
- */
-int main(void)
-{
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof(float));
-	return (0);
+
+int main() {
+   printf("Size of char: %ld byte(s)\n", sizeof(char));
+   printf("Size of short: %ld byte(s)\n", sizeof(short));
+   printf("Size of int: %ld byte(s)\n", sizeof(int));
+   printf("Size of long: %ld byte(s)\n", sizeof(long));
+   printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+   printf("Size of float: %ld byte(s)\n", sizeof(float));
+   printf("Size of double: %ld byte(s)\n", sizeof(double));
+   printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+   printf("Size of pointer: %ld byte(s)\n", sizeof(void *));
+   
+   return 0;
 }
+
