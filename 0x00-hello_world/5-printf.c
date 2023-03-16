daad0216331851ec7b7d2e8096f33c2f3d5b4fc5
ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-03-16 08:59:28 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- The main function description
+ *
+ * Return: 0 for exit
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
