ce83d452605bca92c577e13f91cbaf891d41f76c
ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-03-16 08:49:30 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-The main function description
+ *
+ * Return: 0 for exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
