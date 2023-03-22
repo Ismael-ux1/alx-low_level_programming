b28137cc55d5b3b36ce038ef4291ad51c844378a
ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-03-22 13:22:11 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - This function tests the posive or negative function
+ * Return: This function returns 0 on success
+ */
+int main(void)
+{
+int i;
+i = 88;
+positive_or_negative(i);
+return (0);
+}
