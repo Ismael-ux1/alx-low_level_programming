bd8e0266d55fdba7eb41801b58eee15969e454a1
ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-03-27 16:11:19 -0700

    a program that generates random valid passwords for the program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-#define PASSWORD_LENGTH 10
-#define VALID_CHARACTERS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
 
-int main() {
-    char password[PASSWORD_LENGTH + 1]; // Extra space for null terminator
-    int i, j, valid;
+	sum = 0;	
 
-    srand(time(NULL)); // Initialize random seed
+	srand(time(NULL));
 
-    while (1) { // Loop until a valid password is generated
-        for (i = 0; i < PASSWORD_LENGTH; i++) {
-            j = rand() % strlen(VALID_CHARACTERS); // Get random index
-            password[i] = VALID_CHARACTERS[j]; // Add character to password
-        }
-        password[PASSWORD_LENGTH] = '\0'; // Add null terminator
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-        valid = 1;
-        for (i = 0; i < PASSWORD_LENGTH; i++) {
-            if (password[i] < 33 || password[i] > 126) { // Check if character is outside valid range
-                valid = 0;
-                break;
-            }
-        }
-
-        if (valid) { // If password is valid, output it and exit
-            printf("Valid password: %s\n", password);
-            break;
-        }
-    }
-
-    return 0;
+	return (0);
 }
-
