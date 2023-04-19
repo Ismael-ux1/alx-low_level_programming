24a52bf39d5d091fdaac415a011b57f7ca13c176
ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-04-19 15:31:58 -0700

    a program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the program
- * @argv: array of pointers to the arguments
- *
- * Return: 0 on success, 1 if argc is not 2, or 2 if atoi(argv[1]) is negative
- */
-int main(int argc, char **argv)
+* main - check the code for Holberton School students.
+* @argc: argument count.
+* @argv: argument vector.
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
 {
-int i, bytes;
+char *opc = (char *) main;
+int i, nbytes;
 
 if (argc != 2)
 {
 printf("Error\n");
-return (1);
+exit(1);
 }
 
-bytes = atoi(argv[1]);
+nbytes = atoi(argv[1]);
 
-if (bytes < 0)
+if (nbytes < 0)
 {
 printf("Error\n");
-return (2);
+exit(2);
 }
 
-for (i = 0; i < bytes; i++)
-printf("%02x ", ((unsigned char *)main)[i]);
+for (i = 0; i < nbytes; i++)
+{
+printf("%02x", opc[i] & 0xFF);
+if (i != nbytes - 1)
+printf(" ");
+}
 
 printf("\n");
-
 return (0);
 }
