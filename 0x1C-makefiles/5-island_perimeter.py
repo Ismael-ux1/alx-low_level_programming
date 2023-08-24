9bb73cdbe959a3105c4ad825954f41e94bb85bfa
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-08-23 19:37:07 -0700

    fixed pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,7 +23,7 @@ def island_perimeter(grid):
                 # Check left and up neighbors
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
-                if row > 0 and grid[row - 1][col] ==1:
+                if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2
 
     return perimeter
